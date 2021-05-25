4ae33a6926225008dca2eed67284d27945b53019
zhangyong <zhangyong_web@163.com>
2021-05-25 16:08:38 +0800

    添加.branch.env中配置文件加载

--- README.md
+++ README.md
@@ -3,11 +3,18 @@
  - 用于打包命令相同时，通过分支判断项目运行的环境。
 根据 git 分支，使用 webpack.DefinePlugin 给项目注入不同的环境变量，开发环境时，并不会执行插件。
 
+##### 使用
+- 安装
+```
+npm install --save-dev vue-cli-plugin-define-by-branch
+```
+
  
 - 配置文件
- 文件名：.branch.env.master
+ 文件名：.branch.env/.branch.env.master
  文件内容：
  ```
  VUE_APP_ENV="production"
  ```
  
+ 插件会依次加载 .branch.env 和 .branch.env.{branch} 中的配置。
--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const path = require('path')
 const dotenv = require('dotenv')
 const resolveClientEnv = require('./resolveClientEnv')
 
+function loadEnv(branch) {
+  const envPath = path.resolve(`.branch.env${branch ? `.${branch}` : ``}`)
+  const env = dotenv.config({ path: envPath, debug: process.env.DEBUG })
+  if (env.error) {
+    return {}
+  } else {
+    return env.parsed
+  }
+}
+
 module.exports = (api, options) => {
   if (process.env.NODE_ENV === 'development') {
     return
@@ -14,16 +24,12 @@ module.exports = (api, options) => {
   var currentEnv = resolveClientEnv(options)
 
   try {
-    const envPath = path.resolve(`.branch.env${branch ? `.${branch}` : ``}`)
-    const env = dotenv.config({ path: envPath, debug: process.env.DEBUG })
-    if (env.error) {
-      return
-    }
+    let env = Object.assign(loadEnv(), loadEnv(branch))
     api.chainWebpack(config => {
       config
         .plugin('define')
         .tap(args => {
-          args[0]['process.env'] = JSON.stringify(Object.assign(env.parsed, currentEnv))
+          args[0]['process.env'] = JSON.stringify(Object.assign(currentEnv, env))
           return args
         })
     })
